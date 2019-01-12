b2435e9baeb56d2e969d4cba449f33614c3065f5
Eric Rado <ericvictor14@hotmail.com>
2019-01-12 13:13:45 -0500

    Added model for json response download

---
+++ AxxessCodingChallenge/AxxessCodingChallenge/Models/JSONItem.swift
@@ -0,0 +1,30 @@
+//
+//  JSONEntry.swift
+//  AxxessCodingChallenge
+//
+//  Created by Eric Rado on 1/12/19.
+//
+
+import Foundation
+
+struct JSONItem: Decodable {
+    let id: String
+    let type: String
+    let date: String?
+    let data: String?
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(String.self, forKey: .id)
+        type = try values.decode(String.self, forKey: .type)
+        date = try values.decodeIfPresent(String.self, forKey: .date)
+        data = try values.decodeIfPresent(String.self, forKey: .data)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case type
+        case date
+        case data
+    }
+}
